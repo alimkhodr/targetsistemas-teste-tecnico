c4b880088dab921d60cefac8297f9b2a67dae5c2
alimkhodr <ali.m.khodr@gmail.com>
2025-01-11 00:02:20 -0300

    feat: add exercise 5

---
+++ ex5.js
@@ -0,0 +1,14 @@
+function inverterString(str) {
+    let stringInvertida = "";
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+      stringInvertida += str[i];
+    }
+    
+    return stringInvertida;
+  }
+  
+  const entrada = "exemplo";
+  console.log(`String original: ${entrada}`);
+  console.log(`String invertida: ${inverterString(entrada)}`);
+  
